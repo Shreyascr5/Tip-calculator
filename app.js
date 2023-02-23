c61b86853c03cc95b3b033f6dd7c2d8a88e0bd62
Shreyascr5 <shreyas050801@gmail.com>
2023-02-23 17:03:11 +0530

    resolved the tip value

--- app.js
+++ app.js
@@ -4,14 +4,17 @@ var tipSlider = document.getElementById("tipslider");
 var val = document.getElementById("tipvalue");
 val.innerHTML = tipSlider.value;
 let billrs;
-let tipPercent;
+let tipPer;
+let percentage;
 tipSlider.oninput = function() {
     val.innerHTML = this.value;
     billrs = billinput.value;
-    tipPercent = (val.innerHTML / 100) * billrs;
+    tipPercent = Number((val.innerHTML / 100) * billrs);
+    percentage = tipPercent + Number(billrs);
+    // console.log(percentage);
     if (val.innerHTML == 0) {
         finalBill.innerText = "₹" + billrs;
     } else {
-        finalBill.innerText = "₹" + tipPercent;
+        finalBill.innerText = "₹" + percentage;
     }
 }
